dbb7b750572a5366ccd940778726f2896f865704
Ekalak (Mint) Rengwanidchakul <ekalak.rengwanidchakul@morningstar.com>
2017-12-15 10:11:45 -0600

    Clean up and fmt terraform code

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,4 @@
-
+output "lamba_arn" {
+  description = "Chaos Monkey Lambda ARN"
+  value       = "${aws_lambda_function.chaos_lambda.arn}"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,3 @@
 variable "hook_url" {
-  description = "The Microsoft team hook url, the default one is SRE/Blackbox"
-  default     = "https://outlook.office.com/webhook/a7700941-f64c-467c-945f-6e580241e32e@d2d302fb-0aef-4773-94a5-7950c6f64a35/IncomingWebhook/8573a524e94b40428732d43664402c7c/9c9327e2-0bef-45c3-93d8-099dc24f7be8"
+  description = "The Microsoft team hook url"
 }
